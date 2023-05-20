6ad550a28ac5ff9226c728ba4f05848526d93012
abi-widiarta <abiwidiarta03@gmail.com>
2023-05-20 20:30:42 +0700

    Added selected items outline and disable from picking all items

--- js/script.js
+++ js/script.js
@@ -5,8 +5,21 @@ const allItemsContainer = document.querySelector(".all-items-box-container");
 const boxs = document.querySelectorAll(".box");
 const allItemBoxs = document.querySelectorAll(".box-all");
 
+const algos = document.querySelectorAll(".dropdown-item");
+const chosenAlgoUI = document.querySelector(".chosen-algo");
+
+let chosenAlgo = "Brute force";
+chosenAlgoUI.textContent = chosenAlgo;
+
 let allowSetItem = false;
 let indexItemToSet;
+
+algos.forEach((element) => {
+  element.addEventListener("click", () => {
+    console.log(element.dataset.algo);
+    chosenAlgoUI.textContent = element.dataset.algo;
+  });
+});
 
 function setAllowTrue() {
   allowSetItem = true;
@@ -16,22 +29,39 @@ function setAllowFalse() {
   allowSetItem = false;
 }
 
+let currOutline;
+
 boxs.forEach((element, index) => {
   element.addEventListener("click", () => {
     indexItemToSet = index;
+
+    if (currOutline == undefined) {
+      element.style.outline = "3px solid orange";
+      currOutline = element;
+    } else {
+      currOutline.style.outline = "0px solid orange";
+      element.style.outline = "3px solid orange";
+      currOutline = element;
+    }
+
     setAllowTrue();
     setTest();
   });
 });
 
-function checkExist() {
+function checkExist(srcFromAllItem) {
   let imgSrcArr = [];
+  let exist = false;
 
   boxs.forEach((element) => {
     imgSrcArr.unshift(element.children[0].getAttribute("src"));
   });
 
-  console.log(imgSrcArr);
+  if (imgSrcArr.includes(srcFromAllItem)) {
+    exist = true;
+  }
+
+  return exist;
 }
 
 function setTest() {
@@ -39,9 +69,16 @@ function setTest() {
     allItemBoxs.forEach((element, index) => {
       element.addEventListener("click", () => {
         let chosenItemFromAllItem = element.children[0].getAttribute("src");
-        boxs[indexItemToSet].children[0].setAttribute("src", chosenItemFromAllItem);
         setAllowFalse();
-        checkExist();
+        if (!checkExist(chosenItemFromAllItem)) {
+          boxs[indexItemToSet].children[0].setAttribute("src", chosenItemFromAllItem);
+          element.style.opacity = 0.4;
+          element.style.pointerEvents = "none";
+
+          let boxParent = element.parentElement;
+          boxParent.style.opacity = 0.3;
+          console.log(boxParent);
+        }
       });
     });
   }
